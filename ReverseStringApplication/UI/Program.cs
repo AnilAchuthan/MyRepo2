4a320af1cb288b6e62c8d9f4f8d345551ea3a729
anilachuthan <aanil.net@gmail.com>
2017-11-19 23:11:59 -0500

    Application set up

---
+++ ReverseStringApplication/UI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] name = "Test Name".ToCharArray();
+            string reverse = "";
+            for (int i = name.Length -1; i > -1; i--)
+            {
+                reverse += name[i];    
+            }
+            Console.WriteLine(reverse);
+            Console.ReadLine();
+        }
+    }
+}
